70a396a4a0bf28c36c2c3e1443efb2aecd228b45
Emanuel Santos <emanuel.santos.developer@gmail.com>
2024-03-21 23:16:57 -0300

    Update

---
+++ run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ativação do ambiente virtual
+source /caminho/para/o/seu/ambiente/virtual/venv/bin/activate
+
+# Execução do script Python
+python3 /caminho/para/o/seu/script/meu_script.py
+
+# Desativação do ambiente virtual
+deactivate
